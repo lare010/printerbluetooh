a8a5b2dd4908edc375cf75efb586165b3632900f
lare010 <lare010@gmail.com>
2015-07-26 14:13:49 -0400

    update006

--- www/js/function.js
+++ www/js/function.js
@@ -3,8 +3,8 @@
 $('#connectB').on('click',function(){
 
     bluetoothSerial.list(function(device) {
-        alert(JSON.stringify(device[0]));
-        alert(JSON.stringify(device[1]));
+        alert(JSON.stringify(device[0][0]));
+        alert(JSON.stringify(device[0][1]));
         //alert()
     bluetoothSerial.connect(device[0].address, conexionExito, conexionFallo);
     }, function() {
