98d992d542998928ffe74d9a498487873a36b646
lare010 <lare010@gmail.com>
2015-07-28 22:35:15 -0400

    001

--- www/zbtprinter.js
+++
@@ -1,6 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.print = function(str, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'print', [str]);
-};
-
