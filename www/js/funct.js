def31ec79ab777b31dea54da001265afec5cd0e8
lare010 <lare010@gmail.com>
2015-07-30 06:29:47 -0400

    012

--- www/js/funct.js
+++ www/js/funct.js
@@ -1,15 +1,15 @@
-connectButton1.addEventListener('touchend', mConnection, false);
 
-mConnection: function() {
-	bluetoothSerial.isConnected(
-	    function() {
-	        console.log("Bluetooth is connected");
-	        alert('connected')
-	    },
-	    function() {
-	        console.log("Bluetooth is *not* connected");
-	        alert('notconected')
-	    }
-	);
-}
-alert(2) 
+$('#connectButton1').on('click',function(){
+
+		bluetoothSerial.isConnected(
+		    function() {
+		        console.log("Bluetooth is connected");
+		        alert('connected')
+		    },
+		    function() {
+		        console.log("Bluetooth is *not* connected");
+		        alert('notconected')
+		    }
+		);
+
+})
