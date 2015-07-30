96886103bc42c6bad4f928e5f031ab967279166d
lare010 <lare010@gmail.com>
2015-07-30 06:38:39 -0400

    013

--- www/js/funct.js
+++ www/js/funct.js
@@ -3,12 +3,14 @@ $('#connectButton1').on('click',function(){
 
 		bluetoothSerial.isConnected(
 		    function() {
-		        console.log("Bluetooth is connected");
+		        //console.log("Bluetooth is connected");
 		        alert('connected')
+		        // string
+				bluetoothSerial.write("hello, world", function() {alert('impreso')}, function() {alert('fallotecnico')});
 		    },
 		    function() {
 		        console.log("Bluetooth is *not* connected");
-		        alert('notconected')
+		        //alert('notconected')
 		    }
 		);
 
