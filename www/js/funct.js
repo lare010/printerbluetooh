138be715dcb1cfc6980cf86031712bc3e718ec61
lare010 <lare010@gmail.com>
2015-07-29 22:18:40 -0400

    010

---
+++ www/js/funct.js
@@ -0,0 +1,14 @@
+connectButton1.addEventListener('touchend', mConnection, false);
+
+mConnection: function() {
+	bluetoothSerial.isConnected(
+	    function() {
+	        console.log("Bluetooth is connected");
+	        alert('connected')
+	    },
+	    function() {
+	        console.log("Bluetooth is *not* connected");
+	        alert('notconected')
+	    }
+	);
+}
