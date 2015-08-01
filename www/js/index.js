d3f689e0db922547bdcebe3c8e38646395df2911
lare010 <lare010@gmail.com>
2015-08-01 06:30:53 -0400

    01

--- www/js/index.js
+++ www/js/index.js
@@ -1,150 +1,62 @@
 /*
-    SimpleSerial index.js
-    Created 7 May 2013
-    Modified 9 May 2013
-    by Tom Igoe
-*/
-var macPrinter='';
-var namPrinter='';
-
-var app = {
-    macAddress: "",//"00:01:90:C1:A9:32",  // get your mac address from bluetoothSerial.list
-    chars: "",
-
-/*
-    Application constructor
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+var app = {
+    // Application Constructor
     initialize: function() {
         this.bindEvents();
-        console.log("Starting SimpleSerial app");
     },
-/*
-    bind any events that are required on startup to listeners:
-*/
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener('deviceready', app.manageConnection, false);
-        connectButton.addEventListener('touchend', app.manageConnection, false);
-        cButton.addEventListener('touchend', app.mConnection, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
     },
 
-
-/*
-    Connects if not connected, and disconnects if connected:
-*/
-    mConnection: function() {
-        bluetoothSerial.isConnected(
-            function() {
-                app.display("attempting to disconnect");
-                bluetoothSerial.disconnect(
-                    app.closePort,     // stop listening to the port
-                    app.showError      // show the error if you fail
-                );
-            },
-            function() {
-                console.log("Bluetooth is *not* connected");
-            }
-        );    
-    },
-
-    manageConnection: function() {
-
-        bluetoothSerial.list(
-            function(results) {
-                macPrinter= results[0].address;
-                namPrinter= results[0].name;
-                
-                // here's the real action of the manageConnection function:
-                //bluetoothSerial.isConnected(disconnect(), connect());
-                bluetoothSerial.isConnected(
-                    function() {
-                        //console.log("Bluetooth is connected");
-                        alert('Bluetooth esta conectedo');
-                    },
-                    function() {
-                        // attempt to connect:
-                        bluetoothSerial.connect(
-                            macPrinter,//app.macAddress,  // device to connect to
-                            app.openPort,    // start listening if you succeed
-                            app.showError    // show the error if you fail
-                        );
-                    }
-                );
-            },
-            function(error) {
-               alert(error); //app.display(JSON.stringify(error));
-            }
-        );
-    },
-/*
-    subscribes to a Bluetooth serial listener for newline
-    and changes the button:
-*/
-    openPort: function() {
-        // if you get a good Bluetooth serial connection:
-        app.clear();
-        app.display("Conectado al printer: " +namPrinter+" mac: "+ macPrinter);
-
-        // string
-        bluetoothSerial.write("hello, world", function(){alert('print')} , function(){'not print'});
-
-        // change the button's name:
-        connectButton.innerHTML = "Disconnect";
-        // set up a listener to listen for newlines
-        // and display any new data that's come in since
-        // the last newline:
-        bluetoothSerial.subscribe('\n', function (data) {
-            app.clear();
-            app.display(data);
-        });
-    },
-
-/*
-    unsubscribes from any Bluetooth serial listener and changes the button:
-*/
-    closePort: function() {
-        // if you get a good Bluetooth serial connection:
-        app.clear();
-        app.display("Disconnected from: " +namPrinter+" mac: "+ macPrinter);
-        // change the button's name:
-        connectButton.innerHTML = "Connect";
-        // unsubscribe from listening:
-        bluetoothSerial.unsubscribe(
-                function (data) {
-                    app.display(data);
-                },
-                app.showError
-        );
-    },
-/*
-    appends @error to the message div:
-*/
-    showError: function(error) {
-        // if not connected, do this:
-        // clear the screen and display an attempt to connect
-        app.clear();
-        app.display("El intento de conectarse. " +
-            "Asegurese de que el puerto serie esta abierto en el dispositivo de destino.");
-        app.display(error);
-    },
-
-/*
-    appends @message to the message div:
-*/
-    display: function(message) {
-        var display = document.getElementById("message"), // the message div
-            lineBreak = document.createElement("br"),     // a line break
-            label = document.createTextNode(message);     // create the label
-
-        display.appendChild(lineBreak);          // add a line break
-        display.appendChild(label);              // add the message node
-    },
-/*
-    clears the message div:
-*/
-    clear: function() {
-        var display = document.getElementById("message");
-        display.innerHTML = "";
+    takePicture: function() {
+      navigator.camera.getPicture( function( imageURI ) {
+        alert( imageURI );
+      },
+      function( message ) {
+        alert( message );
+      },
+      {
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI
+      });
     }
-};      // end of app
-
+};
