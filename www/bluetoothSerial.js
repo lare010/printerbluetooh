7e253dee56d620204698a6bee0ca182cc1bf1f9f
lare010 <lare010@gmail.com>
2015-08-01 12:52:13 -0400

    103

--- www/bluetoothSerial.js
+++ www/bluetoothSerial.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-var myDatecsSDK = {
+var BluetoothSerial = {
     platforms: ['android'],
 
     isSupported: function() {
@@ -14,68 +14,68 @@ var myDatecsSDK = {
     },
 
     connect: function(address, onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'connect', [address]);
+        exec(onSuccess, onError, 'BluetoothSerial', 'connect', [address]);
     },
 
     disconnect: function(onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'disconnect', []);
+        exec(onSuccess, onError, 'BluetoothSerial', 'disconnect', []);
     },
 
     init: function(onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'init', []);
+        exec(onSuccess, onError, 'BluetoothSerial', 'init', []);
     },
 
     finish: function(onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'finish', []);
+        exec(onSuccess, onError, 'BluetoothSerial', 'finish', []);
     },
 
     reset: function(onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'reset', []);
+        exec(onSuccess, onError, 'BluetoothSerial', 'reset', []);
     },
 
     printText: function(text, charset, onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'printText', [text, charset]);
+        exec(onSuccess, onError, 'BluetoothSerial', 'printText', [text, charset]);
     },
 
     printTaggedText: function(text, charset, onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'printTaggedText', [text, charset]);
+        exec(onSuccess, onError, 'BluetoothSerial', 'printTaggedText', [text, charset]);
     },
 
     feedPaper: function(lines, onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'feedPaper', [lines]);
+        exec(onSuccess, onError, 'BluetoothSerial', 'feedPaper', [lines]);
     },
 
     flush: function(onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'flush', []);
+        exec(onSuccess, onError, 'BluetoothSerial', 'flush', []);
     },
 
     selectPageMode: function(onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'selectPageMode', []);
+        exec(onSuccess, onError, 'BluetoothSerial', 'selectPageMode', []);
     },
 
     selectStandardMode: function(onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'selectStandardMode', []);
+        exec(onSuccess, onError, 'BluetoothSerial', 'selectStandardMode', []);
     },
 
     printPage: function(onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'printPage', []);
+        exec(onSuccess, onError, 'BluetoothSerial', 'printPage', []);
     },
 
     setAlign: function(align, onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'setAlign', [align]);
+        exec(onSuccess, onError, 'BluetoothSerial', 'setAlign', [align]);
     },
 
     printSelfTest: function(onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'printSelfTest', []);
+        exec(onSuccess, onError, 'BluetoothSerial', 'printSelfTest', []);
     },
     
     printBarcode: function(qrcode, onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'printBarcode', [qrcode]);
+        exec(onSuccess, onError, 'BluetoothSerial', 'printBarcode', [qrcode]);
     },
     
     printImage: function(imagem, onSuccess, onError) {
-        exec(onSuccess, onError, 'MyDatecsSDK', 'printImage', [imagem]);
+        exec(onSuccess, onError, 'BluetoothSerial', 'printImage', [imagem]);
     }
 };
 
-module.exports = myDatecsSDK;
+module.exports = BluetoothSerial;
